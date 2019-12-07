485572a7e13db5cc967be353b6f3f99ef827c6e9
Dave Black <chinweike89@gmail.com>
2019-12-07 21:16:16 +0100

    first commit

---
+++ Desktop/recipe-home/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './components/home/home.js';
+import './App.scss';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ Desktop/recipe-home/src/components/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Landing from './landing';
+
+
+const Home = () => {
+  return(
+    <div className="div">
+      <Landing />
+    </div>
+  );
+}
+
+export default Home;
+
