e3a3c41d76e9822385b661e162376f2f2935c124
Dave Black <chinweike89@gmail.com>
2019-12-11 19:55:32 +0100

    commit

--- Desktop/buyCoinTask/app.js
+++ Desktop/buyCoinTask/app.js
@@ -1,26 +1,44 @@
 const fetchData = async (num) => {
  const apiEndPoint = `https://api.coinlore.com/api/tickers/?start=${num}&limit=10`;
-
  const response = await fetch(apiEndPoint);
  const data = await response.json();
  return data;
 }
 
-const paintDom = (data)=> {
-  data.forEach(item => {
-    return `
-      <tr>
-        <td>${item.name}</td>  
-        <td>${item.symbol}</td>
-        <td>$${item.price_usd}</td>
-        <td>${item.tsupply} ${item.symbol}</td> 
-      </tr>
-    `
-  });
+const htmlElement = (item)=> {
+  const dom =  `
+    <td>${item.name}</td>  
+    <td>${item.symbol}</td>
+    <td>$${item.price_usd}</td>
+    <td>${item.tsupply} ${item.symbol}</td> 
+  `;
+
+  return dom;
+}
+
+const paintDom = (number) => {
+  const tBody = document.querySelector('tbody');
+  tBody.innerHTML = '';
+
+  fetchData(number).then(data => {
+    const result = data.data;
+    
+    result.forEach(item => {
+      let dom = htmlElement(item);
+      const tr = document.createElement('tr');
+      tr.innerHTML = dom;
+      tBody.appendChild(tr)
+    })
+  })
 }
 
 const start = (() => {
-  // const button = document.querySelector();
-  const tBody = document.querySelector('tbody');
-  console.log(tBody);
+  let number = 10;
+  const button = document.querySelector('.btn');
+  paintDom(number);
+
+  button.addEventListener('click', () => {
+    number += 10;
+    paintDom(number);
+  });
 })();
