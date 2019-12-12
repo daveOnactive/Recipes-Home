afcf5920db7ea0c1b670d6a135cf284cd9141e20
Dave Black <chinweike89@gmail.com>
2019-12-12 13:53:40 +0100

    reponsiveness

--- Desktop/buyCoinTask/app.js
+++ Desktop/buyCoinTask/app.js
@@ -1,16 +1,28 @@
 const fetchData = async (num) => {
- const apiEndPoint = `https://api.coinlore.com/api/tickers/?start=${num}&limit=10`;
+ const apiEndPoint = `https://api.coinlore.com/api/tickers/?start=${ num }&limit=10`;
  const response = await fetch(apiEndPoint);
  const data = await response.json();
  return data;
 }
 
 const htmlElement = (item)=> {
-  const dom =  `
-    <td>${item.name}</td>  
-    <td>${item.symbol}</td>
-    <td>$${item.price_usd}</td>
-    <td>${item.tsupply} ${item.symbol}</td> 
+  const dom = `
+    <td>
+      <h3 class="head">💰 Coin</h3>
+      <span>${ item.name }</span>
+    </td>
+    <td>
+      <h3 class="head">📄 Code</h3>
+      <span>${ item.symbol }</span>
+    </td>
+    <td>
+      <h3 class="head">🤑 Price</h3>
+      <span>$ ${ item.price_usd }</span>
+    </td>
+    <td>
+      <h3 class="head">📉 Total Supply</h3>
+      <span>${ item.tsupply } ${ item.symbol }</span>
+    </td>
   `;
 
   return dom;
@@ -27,18 +39,31 @@ const paintDom = (number) => {
       let dom = htmlElement(item);
       const tr = document.createElement('tr');
       tr.innerHTML = dom;
-      tBody.appendChild(tr)
+      tBody.appendChild(tr);
     })
   })
 }
 
+const showBtn = (elem, num) => {
+  num > 10 ? elem.style.visibility = 'visible' : elem.style.visibility = 'hidden';
+}
+
 const start = (() => {
   let number = 10;
-  const button = document.querySelector('.btn');
+  const button = document.querySelector('.btn-next');
+  const preBtn = document.querySelector('.btn-prev');
   paintDom(number);
+  showBtn(preBtn, number);
 
   button.addEventListener('click', () => {
     number += 10;
     paintDom(number);
+    showBtn(preBtn, number);
   });
+
+  preBtn.addEventListener('click', () => {
+    number -= 10;
+    paintDom(number);
+    showBtn(preBtn, number);
+  })
 })();
