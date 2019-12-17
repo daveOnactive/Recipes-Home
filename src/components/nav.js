eafa35fb75884b57437cb97b37d492a37367dbbe
Dave Black <chinweike89@gmail.com>
2019-12-17 19:37:39 +0100

    new logo added

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Logo from './logo';
 import '../styles/nav.scss';
 
 const Nav = () => {
   return (
     <nav>
       <div className="logo">
-        RECIPE-HOMES
+        <Logo />
       </div>
       <div className="nav-links">
         <Link to="/" className="link">
